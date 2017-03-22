d7069501dd5b3da2b8854e75ea040b673221fe5d
Piotr Marczyszyn <pm@miinto.com>
2017-03-22 18:21:00 +0100

    new file:   src/Database/Initializer/Initializer.php

---
+++ src/Database/Initializer/Initializer.php
@@ -0,0 +1,81 @@
+<?php
+
+/**
+ * This class carries ioc initialization functionality used by this component.
+ */
+declare (strict_types=1);
+
+namespace Maleficarum\Database;
+
+class Initializer {
+	/**
+	 * This will setup all IOC definitions specific to this component.
+	 */
+	public function initialize () {
+		\Maleficarum\Ioc\Container::register('Maleficarum\Database\Shard\Manager', function ($dep) {
+			$manager = new \Maleficarum\Database\Shard\Manager();
+
+			// check shard list
+			if (!isset($dep['Maleficarum\Config']['database']['shards']) || !count($dep['Maleficarum\Config']['database']['shards'])) {
+				throw new \RuntimeException('Cannot set up database access - no shards are defined.');
+			}
+
+			// create shard connections
+			$shards = [];
+			foreach ($dep['Maleficarum\Config']['database']['shards'] as $shard) {
+				if (!isset($dep['Maleficarum\Config']['database_shards'][$shard]) || !count($dep['Maleficarum\Config']['database_shards'][$shard])) {
+					throw new \RuntimeException('No config defined for the shard: ' . $shard);
+				}
+				$cfg = $dep['Maleficarum\Config']['database_shards'][$shard];
+				$shards[$shard] = \Maleficarum\Ioc\Container::get('Maleficarum\Database\Shard\Connection\Pgsql\Connection');
+				$shards[$shard]
+					->setHost($cfg['host'])
+					->setPort((int)$cfg['port'])
+					->setDbname($cfg['dbName'])
+					->setUsername($cfg['user'])
+					->setPassword($cfg['password']);
+			}
+
+			// check routes
+			if (!isset($dep['Maleficarum\Config']['database']['routes']) || !count($dep['Maleficarum\Config']['database']['routes'])) {
+				throw new \RuntimeException('Cannot set up database access - no shard routes are defined.');
+			}
+			if (!array_key_exists(\Maleficarum\Database\Shard\Manager::DEFAULT_ROUTE, $dep['Maleficarum\Config']['database']['routes'])) {
+				throw new \RuntimeException('Cannot set up database access - default route is not defined.');
+			}
+
+			// attach shards to routes
+			foreach ($dep['Maleficarum\Config']['database']['routes'] as $route => $shard) {
+				$manager->attachShard($shards[$shard], $route);
+			}
+
+			return $manager;
+		});
+
+		\Maleficarum\Ioc\Container::register('Maleficarum\Database\Shard\Connection\Pgsql\Connection', function () {
+			return (new \Maleficarum\Database\Shard\Connection\Pgsql\Connection());
+		});
+
+		\Maleficarum\Ioc\Container::register('PDO', function ($dep, $opts) {
+			$pdo = new \PDO($opts['dsn']);
+
+			$args = [$pdo];
+			if (isset($dep['Maleficarum\Profiler\Database'])) {
+				$profiler = $dep['Maleficarum\Profiler\Database'];
+				array_push($args, function (array $data) use ($profiler){
+					$profiler->addQuery(
+						$data['start'],
+						$data['end'],
+						$data['queryString'],
+						$data['params']
+					);
+				});
+			}
+
+			$pdo->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
+			$pdo->setAttribute(\PDO::ATTR_STATEMENT_CLASS, ['Maleficarum\Database\PDO\Statement\Profiled', $args]);
+
+			return $pdo;
+		});
+	}
+}
